36563892605e21205cc52bf006abf89ef2c0b59e
aaronj1 <46979125+aaronj1@users.noreply.github.com>
2020-12-09 17:31:51 -0600

    sizing adjustments

--- js/main4.js
+++ js/main4.js
@@ -170,7 +170,7 @@ function initMap() {
 
         /* Set the width of the sidebar to 250px (show it) */
         function openNav() {
-          document.getElementById("mySidepanel").style.width = "250px";
+          document.getElementById("mySidepanel").style.width = "700px";
         }
   
         /* Set the width of the sidebar to 0 (hide it) */
